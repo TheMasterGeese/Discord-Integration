ecb578a2ceb42ff8a8f0386d917606004ddddbb3
TheMasterGeese <themastergeese@gmail.com>
2022-01-30 14:56:05 -0500

    trying to catch hook on opening user information

--- src/DiscordIntegration.ts
+++ src/DiscordIntegration.ts
@@ -1,12 +1,27 @@
-const fGame : Game = game as Game;
 
-Hooks.once("init", function(){
-    fGame.settings.register("discord-integration", "discordServerURL", {
-      name: fGame.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrl"),
-      hint: fGame.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrlHint"),
-      scope: "world",
-      config: true,
-      type: String,
-      default: "",
+Hooks.once("init", function () {
+    (game as Game).settings.register("discord-integration", "discordServerURL", {
+        name: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrl"),
+        hint: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrlHint"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "",
     });
+
+    /*
+    (game as Game).settings.registerMenu("discord-integration", "discordUserIDsDialog", {
+        name: (game as Game).i18n.localize(''),
+        label: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordUserIDsDialogLabel"),
+        hint: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordUserIDsDialogHint"),
+        icon: "fas fa-cog",
+        type: DiscordUserIDSettingsDialog,
+        restricted: true
+    });
+    */
+
+});
+
+Hooks.on("renderUserConfig", function () {
+    console.log("User Config Hook works.")
 });
