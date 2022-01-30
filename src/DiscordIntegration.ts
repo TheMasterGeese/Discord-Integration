673d161ae4c4135bd3e8e3967fed3c3fa9a7cc06
TheMasterGeese <themastergeese@gmail.com>
2022-01-30 17:25:16 -0500

    Added fields to user configuration and module settings

--- src/DiscordIntegration.ts
+++ src/DiscordIntegration.ts
@@ -1,14 +1,14 @@
 
 Hooks.once("init", function () {
-    (game as Game).settings.register("discord-integration", "discordServerURL", {
-        name: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrl"),
-        hint: (game as Game).i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrlHint"),
+    game.settings.register("discord-integration", "discordServerURL", {
+        name: game.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrl"),
+        hint: game.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrlHint"),
         scope: "world",
         config: true,
         type: String,
         default: "",
     });
-
+    
     /*
     (game as Game).settings.registerMenu("discord-integration", "discordUserIDsDialog", {
         name: (game as Game).i18n.localize(''),
@@ -22,6 +22,20 @@ Hooks.once("init", function () {
 
 });
 
-Hooks.on("renderUserConfig", function () {
-    console.log("User Config Hook works.")
+Hooks.on("renderUserConfig", async function (config: UserConfig, element: any, options: Object) {
+    const foundryUser: StoredDocument<User> = game.users!.contents.filter(user => { if (user.id === config.object.data._id) return user; })[0];
+
+    let discordUserId: string = await foundryUser.getFlag('discord-integration', 'discordID') as string 
+    discordUserId = discordUserId ? discordUserId : ""
+
+    let input = `<input type="text" name="${foundryUser.name}" value="${discordUserId}" data-dtype="String">`
+
+
+    const playerColourGroup = element.find('.form-group').eq(2);
+    playerColourGroup.after($(`
+                <div class="form-group discord">
+                    <label>${game.i18n.localize("DISCORDINTEGRATION.UserDiscordIdLabel")}</label>
+                    ${input}
+                </div>
+            `));
 });
