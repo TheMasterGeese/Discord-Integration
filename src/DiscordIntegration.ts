15e701d386c54b8fecb6659d4780945cf37b7ec5
TheMasterGeese <themastergeese@gmail.com>
2022-01-30 09:33:52 -0500

    Add setting in setting menu to set discord server

--- src/DiscordIntegration.ts
+++ src/DiscordIntegration.ts
@@ -1,4 +1,12 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
-}
-hello("TypeScript");
+const fGame : Game = game as Game;
+
+Hooks.once("init", function(){
+    fGame.settings.register("discord-integration", "discordServerURL", {
+      name: fGame.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrl"),
+      hint: fGame.i18n.localize("DISCORDINTEGRATION.SettingsDiscordServerUrlHint"),
+      scope: "world",
+      config: true,
+      type: String,
+      default: "",
+    });
+});
