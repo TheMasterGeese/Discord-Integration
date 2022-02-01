fa599e337a4f7f2d0fc9c4d6a7513b3568d46bd4
TheMasterGeese <themastergeese@gmail.com>
2022-01-31 22:17:56 -0500

    sends messages to json now

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 
-
 gulp.task("transpile", function () {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+    return tsProject.src()
+        .pipe(tsProject()).js
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task("zip", function() {
-    return gulp.src('lang/*', 'dist/*', 'module.json')
-		.pipe(zip('Discord-Integration.zip'))
-		.pipe(gulp.dest('out'))
+    return gulp.src(['lang', 'dist/*', 'module.json'])
+        .pipe(zip('Discord-Integration.zip'))
+        .pipe(gulp.dest('out'))
 });
--- src/DiscordIntegration.ts
+++ src/DiscordIntegration.ts
@@ -39,4 +39,60 @@ Hooks.on("closeUserConfig", async function (config : UserConfig, element : any) 
 
     foundryUser.update({'flags.discord-integration.discordID': discordID}); 
 });
-// hook into when the user config is saved
+
+/**
+ * To forward a message to discord, do one of two things:
+ * 
+ * -include "@<username>" for a user in the game, it will then look up the corresponding discordID 
+ * and send a message pinging them. If you @ multiple people, it will ping all of them. Will not
+ * send a message unless the username matches up with an actual user.
+ * 
+ * -include "@Discord", which will unconditionally forward the message (minus the @Discord) to the Discord Server.
+ * 
+ */
+// whenever someone sends a chat message, if it is marked up properly forward it to Discord.
+
+Hooks.on("chatMessage", function (chatLog : ChatLog, message : string, options : Object) {
+
+    // search for any @<username> strings in the message
+    const userNames : string[]  = game.users!.contents.map( (user) => { return user.name! }); // get a list of usernames
+    let usersToPing : string[] = [];
+    userNames.forEach((userName : string) => {
+        if (message.search(`@${userName}`)) {
+            usersToPing.push(userName);
+        }
+    })
+
+    // search for @Discord in the message
+    let shouldPingDiscord : boolean = (message.search(`@Discord`) !== -1);
+
+    if (userNames.length !== 0) {
+
+        userNames.forEach((userName : string) => {
+            const currentUser : User | undefined = game.users?.contents.filter((user : User)=> { user.name === userName })[0];
+            if (currentUser) {
+                let currentUserDiscordID : string = currentUser.getFlag('discord-integration', 'discordID') as string;
+                message.replace(`@${userName}`, `@${currentUserDiscordID}`)
+            }
+        })
+        sendDiscordMessage(message);
+    } else if (shouldPingDiscord) {
+        // Remove @Discord from the message before sending it.
+        const discordMessage : string | undefined = message.split("@Discord").pop();
+        sendDiscordMessage(discordMessage ? discordMessage : "");
+    }
+
+});
+
+// Send a discord message to the configured URL.
+async function sendDiscordMessage(message : string) {
+    let messageJSON = {
+        "content": message
+    }
+    $.ajax({
+        method: 'POST',
+        url: game.settings.get('discord-integration', 'discordServerURL') as string,
+        contentType: "application/json",
+        data: JSON.stringify(messageJSON)
+    });
+}
