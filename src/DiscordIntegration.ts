f78b32d57bb3adcb776769d78a6897433fefa4ee
TheMasterGeese <themastergeese@gmail.com>
2022-02-07 22:18:37 -0500

    Stashed Change

--- src/DiscordIntegration.ts
+++ src/DiscordIntegration.ts
@@ -1,4 +1,3 @@
-
 Hooks.once("init", function () {
     // add settings option for URL of Discord Webhook
     game.settings.register("discord-integration", "discordWebhook", {
@@ -13,15 +12,19 @@ Hooks.once("init", function () {
 
 // add in the extra field for DiscordID
 Hooks.on("renderUserConfig", async function (config: UserConfig, element: any, options: Object) {
+    // find the user that you're opening config for
     const foundryUser: StoredDocument<User> = game.users!.contents.filter(user => { if (user.id === config.object.data._id) return user; })[0];
 
+    // get their Discord ID if it exists
     let discordUserId: string = await foundryUser.getFlag('discord-integration', 'discordID') as string 
     discordUserId = discordUserId ? discordUserId : ""
 
+    // create the input field to configure it.
     let input = `<input type="text" name="discord-id-config" value="${discordUserId}" data-dtype="String">`
 
-    const playerColourGroup = element.find('.form-group').eq(2);
-    playerColourGroup.after($(`
+    // Put the input field below the "Player Color group" field.
+    const playerColorGroup = element.find('.form-group').eq(2);
+    playerColorGroup.after($(`
                 <div class="form-group discord">
                     <label>${game.i18n.localize("DISCORDINTEGRATION.UserDiscordIdLabel")}</label>
                     ${input}
@@ -32,10 +35,13 @@ Hooks.on("renderUserConfig", async function (config: UserConfig, element: any, o
 // commit any changes to userConfig
 Hooks.on("closeUserConfig", async function (config : UserConfig, element : any) {
 
+    // find the user that the config was open for
     const foundryUser: StoredDocument<User> = game.users!.contents.filter(user => { if (user.id === config.object.data._id) return user; })[0];
 
+    // get the value of the discord id field.
     let discordID : string = element.find("input[name = 'discord-id-config']")[0].value;
 
+    // update the flag
     foundryUser.update({'flags.discord-integration.discordID': discordID}); 
 });
 
@@ -49,8 +55,8 @@ Hooks.on("closeUserConfig", async function (config : UserConfig, element : any) 
  * -include "@Discord", which will unconditionally forward the message (minus the @Discord) to the Discord Webhook.
  * 
  */
-// whenever someone sends a chat message, if it is marked up properly forward it to Discord.
 
+// whenever someone sends a chat message, if it is marked up properly forward it to Discord.
 Hooks.on("chatMessage", async function (chatLog : ChatLog, message : string, options : Object) {
 
     // search for any @<username> strings in the message
@@ -65,6 +71,7 @@ Hooks.on("chatMessage", async function (chatLog : ChatLog, message : string, opt
     // search for @Discord in the message
     let shouldPingDiscord : boolean = (message.search(`@Discord`) !== -1);
 
+    // if it found any @<username> values, replace the values in the message with appropriate discord pings, then send discord message.
     if (usersToPing.length !== 0) {
 
         usersToPing.forEach((userName : string) => {
@@ -75,15 +82,22 @@ Hooks.on("chatMessage", async function (chatLog : ChatLog, message : string, opt
             }
         })
         await sendDiscordMessage(message);
+    // else if Discord as a whole is being pinged, remove the "@Discord" part and then send the message.
     } else if (shouldPingDiscord) {
-        // Remove @Discord from the message before sending it.
         const discordMessage : string | undefined = message.split("@Discord").pop();
         await sendDiscordMessage(discordMessage ? discordMessage : "");
     }
 
 });
 
-// Send a discord message to the configured URL.
+
+/**
+ * Sends a message through the discord webhook as configured in settings.
+ * 
+ * Messages that ping users in Discord need to have "@<gameUserName>" and the users must have their discord IDs configured.
+ * 
+ * @param message The message to forward to Discord
+ */
 export async function sendDiscordMessage(message : string) {
     let messageJSON = {
         "content": message
